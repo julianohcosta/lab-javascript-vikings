6feef4f878ccb477272375de43f01489276d52b7
Juliano Hausen Oliveira da Costa <juliano.h.costa@rfb.gov.br>
2022-10-24 16:40:06 -0400

    solved lab

--- src/viking.js
+++ src/viking.js
@@ -153,7 +153,7 @@ class War {
       return `Vikings have won the war of the century!`
     } else if (this.vikingArmy.length === 0) {
       return `Saxons have fought for their lives and survived another day...`
-    }else if (this.saxonArmy.length === 1 && this.vikingArmy.length === 1){
+    }else if (this.saxonArmy.length > 1 && this.vikingArmy.length > 1){
       return `Vikings and Saxons are still in the thick of battle.`
     }
   }
